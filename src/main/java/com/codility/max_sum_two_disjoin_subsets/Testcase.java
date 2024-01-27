39a8b445f6478b43cd3db398c65bca7331d4aee6
morpheus9631 <morpheus.hsieh@gmail.com>
2024-01-27 20:48:53 +0800

    Add test of Max um Two Disjoin Subsets (#14)

---
+++ src/main/java/com/codility/max_sum_two_disjoin_subsets/Testcase.java
@@ -0,0 +1,14 @@
+package com.codility.max_sum_two_disjoin_subsets;
+
+import lombok.*;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Testcase 
+{
+    private int[] nums;
+    private int K;
+    private int L;
+}
