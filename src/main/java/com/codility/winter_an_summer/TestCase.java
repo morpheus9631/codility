bff902eddcd69080982d15d693554bd1b4b33971
morpheus9631 <morpheus.hsieh@gmail.com>
2024-01-28 21:23:44 +0800

    Add unit test of Winter and Summer (#23)

---
+++ src/main/java/com/codility/winter_an_summer/TestCase.java
@@ -0,0 +1,12 @@
+package com.codility.winter_an_summer;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class TestCase 
+{
+  private int[] nums;
+  private int expected;
+}
