5183dd8447782b1dbb67bab5e202f19eb770b7c3
morpheus9631 <morpheus.hsieh@gmail.com>
2024-01-28 17:01:10 +0800

    Add unit test of Sort array with single swap

---
+++ src/main/java/com/codility/sort_array_with_single_swap/TestCase.java
@@ -0,0 +1,11 @@
+package com.codility.sort_array_with_single_swap;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class TestCase {
+    private int[] nums;
+    private boolean expected;
+}
